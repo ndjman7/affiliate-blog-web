bd3d66e6671dde3b4ce8c721fcb0b01d6e39bb68
pando <ndjman7@gmail.com>
2025-06-09 16:31:27 +0900

    fix: 린트 수정

--- src/components/google-ad.tsx
+++ src/components/google-ad.tsx
@@ -7,7 +7,7 @@ export default function GoogleAd() {
   useEffect(() => {
     try {
       // 광고 스크립트를 초기화
-      // @ts-ignore
+      // @ts-expect-error: adsbygoogle는 글로벌 스코프에 존재하지만 타입 정의가 없음
       (window.adsbygoogle = window.adsbygoogle || []).push({});
     } catch (e) {
       console.error("AdSense error", e);
