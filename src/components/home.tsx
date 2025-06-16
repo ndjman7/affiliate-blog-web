ec8e0cb5c50ef8ebad376dae95fdb4f9ec419a28
pando <ndjman7@gmail.com>
2025-06-16 10:26:38 +0900

    운영 환경에서만 GTM 호출 및 API 호출량 감소 (#3)
    * feat: prefetch=false 적용
    
    * feat: 운영 환경에서만 GTM 적용

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -56,6 +56,7 @@ export function Home() {
             <Link
               key={content.id}
               href={`/contents/${content.id}`}
+              prefetch={false}
               className="group block bg-white rounded-lg shadow hover:shadow-md transition p-4 border border-gray-100 hover:border-blue-300"
             >
               <div className="flex items-center">
