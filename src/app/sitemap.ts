6c27fa2cebb83434c9858fd03d1d7f110cd77513
pando <ndjman7@gmail.com>
2025-07-06 20:45:32 +0900

    sitemap.xml api 오류 대응 (#15)

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,9 +11,7 @@ interface SitemapResponseSchema {
   results: ContentSchema[];
 }
 
-const getSitemapContent = (): Promise<
-  AxiosResponse<SitemapResponseSchema>
-> => {
+const getSitemapContent = (): Promise<AxiosResponse<SitemapResponseSchema>> => {
   return axios.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/sitemap/content`);
 };
 
@@ -31,13 +29,18 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   //   },
   // ];
 
-  const { data } = await getSitemapContent();
+  try {
+    const { data } = await getSitemapContent();
 
-  const dynamicUrls = data.results.map((content) => ({
-    url: content.url,
-    lastModified: new Date(content.datetime),
-  }));
+    const dynamicUrls = data.results.map((content) => ({
+      url: content.url,
+      lastModified: new Date(content.datetime),
+    }));
 
-  // 정적 및 동적 URL을 합쳐서 반환합니다.
-  return dynamicUrls;
+    // 정적 및 동적 URL을 합쳐서 반환합니다.
+    return dynamicUrls;
+  } catch (error) {
+    console.error("사이트맵 데이터를 가져오는 중 오류 발생:", error);
+    return []; // 오류 발생 시 빈 배열 반환
+  }
 }
